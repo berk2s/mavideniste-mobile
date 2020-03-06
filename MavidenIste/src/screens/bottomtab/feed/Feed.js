3cff9ada5204cb45c31b822323a9ae943b06c60a
berk2s <berktopcu@outlook.com>
2020-03-06 18:23:53 +0300

    notificitions ios

--- MavidenIste/src/screens/bottomtab/feed/Feed.js
+++ MavidenIste/src/screens/bottomtab/feed/Feed.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import { Container, Header, Button, Content, } from "native-base";
-
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
 // API
 import API from '../../../api'
 
@@ -21,6 +21,9 @@ import Switcher from '../switcher/Switcher';
 import SwitcherStore from '../../../store/SwitcherStore';
 import Spinner from 'react-native-loading-spinner-overlay';
 
+import PushNotification from 'react-native-push-notification'
+import firebase from 'react-native-firebase';
+
 @observer
 export default class Feed extends Component {
 
@@ -33,6 +36,7 @@ export default class Feed extends Component {
 
     componentDidMount = async () => {
         try{
+
             const categories = await API.get(`/api/category/current/${BRANCH_ID}`);
             this.state.datas = [...categories.data.data]
             /*
@@ -46,7 +50,14 @@ export default class Feed extends Component {
                 });
             }, 1000)
 
-            console.log(categories.data.data)
+            console.log(categories.data.data);
+
+          //  PushNotification.localNotification({
+                /* iOS and Android properties */
+          //      title: "İndirim günü!", // (optional)
+          //      message: "7 Mart 03:00'e kadar yapacağınız alışverişlerde yüzde 20 indirim!", // (required)
+          //  });
+
         }catch(e){
             console.log(e)
         }
@@ -138,6 +149,69 @@ export default class Feed extends Component {
     );
   }
 }
+
+const messaging = firebase.messaging();
+
+messaging.hasPermission()
+    .then((enabled) => {
+        if (enabled) {
+            messaging.getToken()
+                .then(token => { console.log('token'); console.log(token); console.log('token') })
+                .catch(error => { /* handle error */ });
+        } else {
+            messaging.requestPermission()
+                .then(() => { /* got permission */ })
+                .catch(error => { /* handle error */ });
+        }
+    })
+    .catch(error => { /* handle error */ });
+firebase.notifications().onNotification((notification) => {
+    const { title, body } = notification;
+    PushNotification.localNotification({
+        title: title,
+        message: body, // (required)
+    });
+});
+
+
+PushNotification.configure({
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function(token) {
+        console.log("TOKEN:", token);
+    },
+
+    // (required) Called when a remote or local notification is opened or received
+    onNotification: function(notification) {
+        console.log("NOTIFICATION:", notification);
+
+        // process the notification
+        alert('x')
+        // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+    // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
+   // senderID: "YOUR GCM (OR FCM) SENDER ID",
+
+    // IOS ONLY (optional): default: all - Permissions to register.
+    permissions: {
+        alert: true,
+        badge: true,
+        sound: true
+    },
+
+    // Should the initial notification be popped automatically
+    // default: true
+    popInitialNotification: true,
+
+    /**
+     * (optional) default: true
+     * - Specified if permissions (ios) and token (android and ios) will requested or not,
+     * - if not, you must call PushNotificationsHandler.requestPermissions() later
+     */
+    requestPermissions: true
+});
+
 
 const styles = StyleSheet.create({
     cardArea:{
